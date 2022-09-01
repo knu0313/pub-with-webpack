eda0c7abb6910f44d73a59956bb1cff30dea3357
unknown <knu0313@gmail.com>
2022-09-01 16:49:17 +0900

    partials 정리

--- src/_helpers/times.js
+++ src/_helpers/times.js
@@ -1,5 +1,11 @@
+/**
+ * 
+ * @param {*} n count
+ * @param {*} block 
+ * @returns string html
+ * {{#times n}}block{{/ times}}
+ */
 module.exports = function (n, block) {
-    console.log('times')
     let accum = '';
     for (let i = 0; i < n; ++i) accum += block.fn(i);
     return accum;
